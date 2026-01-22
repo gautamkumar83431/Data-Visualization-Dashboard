f434176b18be413b51234e0e068190576aa8aa11
Gautam Kumar <gautamkumar83431@gmail.com>
2026-01-22 22:44:00 +0530

    fix build script

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,5 +1,7 @@
+
+const BASE_URL = "https://data-visualization-dashboard-qhpk.onrender.com";
+
 export const fetchInsights = async () => {
-    const res = await fetch("http://localhost:5000/api/insights");
-    return res.json();
-  };
-  
+  const res = await fetch(`${BASE_URL}/api/insights`);
+  return res.json();
+};
